19b0cddf2a8be0ad47aa9c7a30200aba3acdf635
PintilieGeani <pintilie.geany@yahoo.it>
2025-04-29 10:21:20 +0200

    feat: correzione-olga

--- js/script.js
+++ js/script.js
@@ -3,24 +3,27 @@ const button = document.querySelector(".btn")
 let acceso = false
 let body = document.querySelector("body")
 
-button.addEventListener("click", function(){
+
+button.addEventListener("click", toggleLight)
+
+
+function toggleLight(){
     console.log("premo il pulsante")
+    // button.classList.toggle("bg-red")
+    body.classList.toggle("bg-black")
     if(acceso === false){
-        console.log("accendo la luce")
-        image.src = "img/yellow_lamp 1.png"
-        button.classList.toggle("bg-red")
-        body.classList.toggle("bg-black")
+        // console.log("accendo la luce")
+        image.src = "./img/yellow_lamp 1.png"
+        // button.classList.add("bg-red")
+        // body.classList.remove("bg-black")
         button.innerText = "Spegni la luce"
         acceso = true
     }else {
-        image.src = "img/white_lamp 1.png"
-        console.log("spengo la luce")
-        button.classList.toggle("bg-red")
-        body.classList.toggle("bg-black")
+        image.src = "./img/white_lamp 1.png"
+        // console.log("spengo la luce")
+        // button.classList.remove("bg-red")
+        // body.classList.add("bg-black")
         button.innerText = "Accendi la luce"
         acceso = false
     }
-})
-
-
-console.log(image.src)
+}
